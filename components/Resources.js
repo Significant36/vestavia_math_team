10e910fdd5b0b00346e20db20a907e27fc492cb2
threpoint1four <eric.chenan.wang@gmail.com>
2025-08-06 22:13:59 -0500

    fix apostrophe

--- components/Resources.js
+++ components/Resources.js
@@ -1,24 +1,27 @@
 export default function Resources() {
-    return (
-      <section id="resources" className="section">
-        <h2>Resources</h2>
-        <div className="cards">
-          <div className="card">
-            <h3>Practice Materials</h3>
-            <p>Check the Google Classroom for AMC/AIME resources and books</p>
-            <p>See past MAO tests <a href="https://mualphatheta.org/past_tests"> here</a> and past FAMAT tests <a href = "https://famat.org/test-bank/"> here</a></p>
-          </div>
-          <div className="card">
-            <h3>Infinite source of knowledge</h3>
-            <p>Check out Mr. Taylor's bookshelf</p>
-            <p>Or Mrs. Hyde's big office shelves</p>
-          </div>
-          <div className="card">
-            <h3>Middle School Summer Camp</h3>
-            <p>Fundraiser for the VHHS Math Team</p>
-            <p>Specialized classes for various topics</p>
-          </div>
+  return (
+    <section id="resources" className="section">
+      <h2>Resources</h2>
+      <div className="cards">
+        <div className="card">
+          <h3>Practice Materials</h3>
+          <p>Check the Google Classroom for AMC/AIME resources and books</p>
+          <p>
+            See past MAO tests <a href="https://mualphatheta.org/past_tests">here</a> and past FAMAT tests{' '}
+            <a href="https://famat.org/test-bank/">here</a>
+          </p>
         </div>
-      </section>
-    )
-  }
+        <div className="card">
+          <h3>Infinite source of knowledge</h3>
+          <p>Check out the bookshelf</p>
+          <p>Or Mrs. Hyde&rsquo;s big office shelves</p>
+        </div>
+        <div className="card">
+          <h3>Middle School Summer Camp</h3>
+          <p>Fundraiser for the VHHS Math Team</p>
+          <p>Specialized classes for various topics</p>
+        </div>
+      </div>
+    </section>
+  )
+}
