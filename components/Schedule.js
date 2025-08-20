ebb42024f6444d3962e4ba68f6d6218a393d2ffa
Significant36 <106559953+Significant36@users.noreply.github.com>
2025-08-20 14:18:55 -0500

    Update Schedule.js
    added "at/in" prep for loc

--- components/Schedule.js
+++ components/Schedule.js
@@ -4,23 +4,23 @@ export default function Schedule() {
         <h2>Our Tournament Schedule</h2>
         <div className="schedule-item">
           <div className="schedule-date">October 7, 2025</div>
-          <p><strong>Oklahoma State University</strong> At VHHS</p>
+          <p><strong>Oklahoma State University</strong> at VHHS</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">October 25, 2025</div>
-          <p><strong>University of Georgia</strong> Athens, GA</p>
+          <p><strong>University of Georgia</strong> in Athens, GA</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">November 5, 2025</div>
-          <p><strong>AMC 10/12 A</strong> VHHS</p>
+          <p><strong>AMC 10/12 A</strong> at VHHS</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">November 13, 2025</div>
-          <p><strong>AMC 10/12 B</strong> VHHS</p>
+          <p><strong>AMC 10/12 B</strong> at VHHS</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">November 15, 2025</div>
-          <p><strong>Rockdale</strong> Conyers, GA</p>
+          <p><strong>Rockdale</strong> in Conyers, GA</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">February 28, 2026</div>
@@ -28,11 +28,11 @@ export default function Schedule() {
         </div>
         <div className="schedule-item">
           <div className="schedule-date">TBD</div>
-          <p><strong>Clemson Calculus Challenge</strong> Clemson, SC</p>
+          <p><strong>Clemson Calculus Challenge</strong> in Clemson, SC</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">June 28 - July 3, 2026</div>
-          <p><strong>Mu Alpha Theta National Convention</strong> Frisco, TX</p>
+          <p><strong>Mu Alpha Theta National Convention</strong> in Frisco, TX</p>
         </div>
       </section>
     )
