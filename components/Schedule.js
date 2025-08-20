be39a6895324d3d0813e484f32c6ea7433280c64
Significant36 <106559953+Significant36@users.noreply.github.com>
2025-08-20 08:14:00 -0500

    Update Schedule.js

--- components/Schedule.js
+++ components/Schedule.js
@@ -4,22 +4,34 @@ export default function Schedule() {
         <h2>Our Tournament Schedule</h2>
         <div className="schedule-item">
           <div className="schedule-date">TBD</div>
-          <p><strong>TBD</strong> TBD</p>
+          <p><strong>Oklahoma State University</strong> October 7, 2025</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">TBD</div>
-          <p><strong>TBD</strong> TBD</p>
+          <p><strong>University of Georgia</strong> October 25, 2025</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">TBD</div>
-          <p><strong>TBD</strong> TBD</p>
+          <p><strong>AMC 10/12 A</strong> November 5, 2025</p>
         </div>
         <div className="schedule-item">
           <div className="schedule-date">TBD</div>
-          <p><strong>TBD</strong> TBD</p>
+          <p><strong>AMC 10/12 B</strong> November 13, 2025</p>
         </div>
         <div className="schedule-item">
-          <div className="schedule-date">July 2025</div>
+          <div className="schedule-date">TBD</div>
+          <p><strong>Rockdale (Conyers, GA)</strong> November 15, 2025</p>
+        </div>
+        <div className="schedule-item">
+          <div className="schedule-date">TBD</div>
+          <p><strong>Alabama Statewide Math Contest</strong> February 28, 2026</p>
+        </div>
+        <div className="schedule-item">
+          <div className="schedule-date">TBD</div>
+          <p><strong>Clemson Calculus Challenge</strong> TBD</p>
+        </div>
+        <div className="schedule-item">
+          <div className="schedule-date">June 28 - July 3, 2026</div>
           <p><strong>Mu Alpha Theta National Convention</strong> Frisco, TX</p>
         </div>
       </section>
