962404f0a9dae1280e3ef3ba6d6de5c02bbcaba9
Significant36 <106559953+Significant36@users.noreply.github.com>
2025-08-20 08:06:02 -0500

    Update TournamentRegistration.js

--- components/TournamentRegistration.js
+++ components/TournamentRegistration.js
@@ -5,23 +5,23 @@ export default function TournamentRegistration() {
         <div className="cards">
           <div className="card">
             <h3>Tournament Details</h3>
-            <p><strong>Date:</strong> December TBD, 2025</p>
+            <p><strong>Date:</strong> December 6, 2025</p>
             <p><strong>Time:</strong> 8:00 AM - 2:00 PM</p>
             <p><strong>Location:</strong> Vestavia Hills High School</p>
             <p><strong>Registration Deadline:</strong> TBD </p>
+          </div>
+          <div className="card">
+            <h3>Registration Info</h3>
+            <p><strong>Entry Fee:</strong> $10</p>
+            <p><strong>Contact:</strong> Susan Gifford at <a href="giffordms@vhcs.us">giffordms@vhcs.us</a></p>
+            <p><strong>Registration Form:</strong> Register <a href="https://docs.google.com/forms/d/e/1FAIpQLSe-hSfT0VLZIbvFt680wRZyjLamw0a67nZR6I38vZZR5r8m3Q/viewform?usp=sharing&ouid=114059588434101076176">here</a> by December 1, 2025</p>
           </div>
           <div className="card">
             <h3>Competition Categories</h3>
             <p><strong>Levels:</strong> 6th Grade, Prealgebra, Algebra I, Geometry, Algebra II, & Comprehensive</p>
             <p><strong>Format:</strong> Ciphering & Written tests for each level</p>
             <p><strong>Awards: </strong> Cool glass trophies for top performers in each category and division</p>
-            <p><strong>Misc:</strong> Refreshments and lunch offered to all participants in the Cafeteria</p>
-          </div>
-          <div className="card">
-            <h3>Registration Info</h3>
-            <p><strong>Entry Fee:</strong> TBD</p>
-            <p><strong>Contact:</strong> TBD</p>
-            <p><strong>Registration Form:</strong> TBD </p>
+            <p><strong>Misc:</strong> Snacks offered to all participants in the Cafeteria</p>
           </div>
         </div>
       </section>
